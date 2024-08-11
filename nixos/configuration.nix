a36e398c3e1ecb4052c4cf8994aefe09a7fbe3b0
shibahaczix <shibahaczix@gmail.com>
2024-08-11 19:56:49 +0200

    removed key

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,7 +10,6 @@
         "https://cache.nixos.org/"
       ];
       trusted-public-keys = [
-        "binarycache.example.com-1:dsafdafDFW123fdasfa123124FADSAD"
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };

829a9c7315823b95bbb3187c66129639a037a802
shibahaczix <shibahaczix@gmail.com>
2024-08-12 14:11:25 +0200

    fixed fastfetch

--- home/fastfetch.nix
+++ home/fastfetch.nix
@@ -7,32 +7,18 @@
       logo = {
         type = "kitty-direct";
         source = "/home/shiba/flake/home/fastfetch/image.png";
-	width = "36";
-	height = "16";
-	modules = [
-	  {
-	    type = "title";
-	  }
-	  {
-	    type = "separator";
-	  }
-	  {
-	    type = "os";
-	  }
-	  {
-	    type = "kernel";
-	  }
-	  {
-	    type = "uptime";
-	  }
-	  {
-	    type = "packages";
-	  }
-	  {
-	    type = "colors";
-	  }
-	];
+	width = 36;
+	height = 16;
       };
+	modules = [
+	"title"
+	"separator"
+	"os"
+	"kernel"
+	"uptime"
+	"packages"
+        "colors"
+      ];
     };
   };
 }

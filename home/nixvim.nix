a80286406bf0ebd6f62c41fe3fe1151bd7574676
shibahaczix <shibahaczix@gmail.com>
2024-08-18 03:47:32 +0200

    I have no idea (NixVim)

--- home/nixvim.nix
+++ home/nixvim.nix
@@ -17,19 +17,111 @@
     };
     plugins = {
       auto-save.enable = true;
+
       telescope.enable = true;
+
+      oil = {
+        enable = true;
+      };
+      cmp-emoji = {
+        enable = true;
+      };
+
       cmp = {
         enable = true;
-	autoEnableSources = true;
+        settings = {
+          autoEnableSources = true;
+          experimental = { ghost_text = true; };
+          performance = {
+            debounce = 60;
+            fetchingTimeout = 200;
+            maxViewEntries = 30;
+          };
+          snippet = { expand = "luasnip"; };
+          formatting = { fields = [ "kind" "abbr" "menu" ]; };
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "emoji"; }
+            {
+              name = "buffer"; # text within current buffer
+              option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+              keywordLength = 3;
+            }
+            {
+              name = "path"; # file system paths
+              keywordLength = 3;
+            }
+            {
+              name = "luasnip"; # snippets
+              keywordLength = 3;
+            }
+          ];
+
+          window = {
+            completion = { border = "solid"; };
+            documentation = { border = "solid"; };
+          };
+
+          mapping = {
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            "<C-j>" = "cmp.mapping.select_next_item()";
+            "<C-k>" = "cmp.mapping.select_prev_item()";
+            "<C-e>" = "cmp.mapping.abort()";
+            "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+          };
+        };
       };
+      cmp-nvim-lsp = {
+        enable = true; # LSP
+      };
+      cmp-buffer = {
+        enable = true;
+      };
+      cmp-path = {
+        enable = true; # file system paths
+      };
+      cmp_luasnip = {
+        enable = true; # snippets
+      };
+      cmp-cmdline = {
+        enable = true; # autocomplete for cmdline
+      }; 
       #lightline.enable = true;
-      floaterm.enable = true;
+      toggleterm = {
+        enable = true;
+        settings = {
+          hide_numbers = false;
+          autochdir = true;
+          close_on_exit = true;
+          direction = "vertical";
+        };
+      };
+
+      todo-comments = {
+        enable = true;
+        colors = {
+          error = ["DiagnosticError" "ErrorMsg" "#DC2626"];
+          warning = ["DiagnosticWarn" "WarningMsg" "#FBBF24"];
+          info = ["DiagnosticInfo" "#2563EB"];
+          hint = ["DiagnosticHint" "#10B981"];
+          default = ["Identifier" "#7C3AED"];
+          test = ["Identifier" "#FF00FF"];
+        };
+      };
+
       indent-blankline.enable = true;
+
       undotree.enable = true;
+
       lspkind = {
         enable = true;
         mode = "symbol_text";
       };
+
       fidget = {
         enable = true;
          notification = {
@@ -39,6 +131,7 @@
           };
         };
       };
+
       lsp = {
         enable = true;
 
@@ -50,6 +143,7 @@
           };
           rust-analyzer = {
             enable = true;
+            installRustc = true;
             installCargo = true;
           };
           zls = {
@@ -58,6 +152,7 @@
         };
       };
     };
+
     keymaps = [
       {
         key = "<C-/>";
